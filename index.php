57ccbc8d07be6b2ff28fd89e6433a28930c60da8
Vogusov <vogusov@gmail.com>
2020-08-25 15:32:09 +0300

    lesson2 1st commit

--- index.php
+++ index.php
@@ -1,74 +1,184 @@
 <?php
-	$title = 'MinimallisticA';
-	$h1 = 'Greeetings boys and girls! Are you ready to rock\'n\'roll?!';
-	
-	// еще задания:
-	$a = 5;
-	$b = '05';
-	var_dump($a == $b);         // строка при сравнении приводится приводится к числу 5;
-	var_dump((int)'012345');     // строка приводится к целому числу, 0 отбрасывается;
-	var_dump((float)123.0 === (int)123.0); // float строго не равен int;
-	var_dump((int)0 === (int)'hello, world'); // потому что эта строка не может быть приведена к целому числу, то возвращается false; false приводится к 0. 0===0; 
 
-	// 2.
-	$x = 3;
-	$y = 5;
-	$x +=$y;
-	$y = $x - $y;
-	$x -= $y;
-	echo '</br>';
-	echo "x = $x, y = $y";
-?>
+echo '1 задание:<br>';
+/*
+ Объявить две целочисленные переменные $a и $b и задать им произвольные начальные значения. Затем написать скрипт, который работает по следующему принципу:
+если $a и $b положительные, вывести их разность;
+если $а и $b отрицательные, вывести их произведение;
+если $а и $b разных знаков, вывести их сумму;
+*/
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="author" content="Luka Cvrk (www.solucija.com)" />
-	<link rel="stylesheet" href="css/main.css" type="text/css" />
-	<title><?php echo $title ?></title>
-</head>
-<body>
-	<div id="content">
-		<h1><? echo $h1 ?></h1>
-		
-		<ul id="menu">
-			<li><a href="#">home</a></li>
-			<li><a href="#">archive</a></li>
-			<li><a href="#">contact</a></li>
-		</ul>
+function foo() {
+	$a = rand(-10000, 10000);
+	$b = rand(-10000, 10000);
+
+	if ($a >= 0 && $b >= 0) {
+		echo "a и b не отрицательные. Выводим их разность: $a - $b = ".($a - $b);
+	} elseif ($a < 0 && $b < 0) {
+		echo "a и b отрицательные. Выводим их произведение: $a * $b = ".($a * $b);
+	} else {
+		echo "a и b разных знаков. Выводим их сумму: $a + $b = ".($a + $b);
+	}
+}
+
+foo();
+
+
+
+echo '<hr>';
+echo '2 задание:<br>';
+/*
+ Присвоить переменной $а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от $a до 15.
+*/
+
+function foo2($r){	
+
+	switch($r) {
+		case (15):
+			echo $r;
+			break;
+		case ($r < 15):
+			echo "$r ";
+			$r++;
+			foo2($r);
+		break;	
+	}	
+}
+
+foo2(rand(0, 15));
+
+
+
+echo '<hr>';
+echo '3 задание:<br>';
+/*
+Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return
+*/
+function get_sum($a, $b){
+	return $a + $b;
+}
+
+function get_multiplication($a, $b){
+	return $a*$b;
+}
+
+function get_division($a, $b){
+	return round($a/$b, 3);
+}
+
+function get_substraction($a, $b){
+	return $a-$b;
+}
+
+echo "a = 5, b = 3<br>";
+
+echo "5 + 3 = ".get_sum(5, 3)."<br>";
+echo "5 * 3 = ".get_multiplication(5, 3)."<br>";
+echo "5 / 3 = ".get_division(5, 3)."<br>";
+echo "5 - 3 = ".get_substraction(5, 3)."<br>";
+
+
+
+echo '<hr>';
+echo '4 задание:<br>';
+/*
+Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation), где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции. В зависимости от переданного значения операции выполнить одну из арифметических операций (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).  
+*/
+
+function mathOperation($arg1, $arg2, $operation){
+	switch($operation){
+		case("sum"):
+			return get_sum($arg1, $arg2);
+		break;
+
+		case("multiplication"):
+			return get_multiplication($arg1, $arg2);
+		break;
+
+		case("division"):
+			return get_division($arg1, $arg2);
+		break;
+
+		case("substraction"):
+			return get_substraction($arg1, $arg2);
+		break;
+	}
+}
+
+echo "8 + 4 = ".mathOperation(8, 4, 'sum')."<br>";
+echo "8 * 4 = ".mathOperation(8, 4, 'multiplication')."<br>";
+echo "8 / 4 = ".mathOperation(8, 4, 'division')."<br>";
+echo "8 - 4 = ".mathOperation(8, 4, 'substraction')."<br>";
+
+
+
+echo '<hr>';
+echo '*4 задание:<br>';
+/*
+С помощью рекурсии организовать функцию возведения числа в степень. Формат: function power($val, $pow), где $val – заданное число, $pow – степень.
+*/
+ 
+function power($val, $pow){
+	if ($val === 1) {
+		return 1;
+	} elseif ($val === 0) {
+		return 0;
+	}	elseif ($pow === 0) {
+		return 1;
+	} elseif ($pow < 0) {
+		return power(1/$val, abs($pow));
+	} else
+	return $val * power($val, --$pow);
+
+}
+
+
+echo power(3, 3)."<br>";
+echo power(5, -4)."<br>";
+
+
+
+echo '<hr>';
+echo '*5 задание:<br>';
+/*
+*Написать функцию, которая вычисляет текущее время и возвращает его в формате с правильными склонениями, например: 22 часа 15 минут, 21 час 43 минуты.
+*/
+function get_minutes_name($minutes){
+	if ($minutes == 1 || $minutes == 21 || $minutes == 31 || $minutes == 41 || $minutes == 51)
+	{	
+		return $minutes_name = 'минута';
+	}	
+	elseif ( ($minutes[-1] == 2 || $minutes[-1] == 3 || $minutes[-1] == 4) &&
+					($minutes < 12 || $minutes > 14 )	)
+	{
+		return $minutes_name = 'минуты';
+	}
+	else 
+	{
+		return $minutes_name = 'минут';		
+	}
+}
+
+function get_hours_name($hours){
+	if ($hours == 1 || $hours == 21)
+	{
+		return $hours_name = 'час';
+	}
+	elseif ( ($hours[-1] == 2 || $hours[-1] == 3 || $hours[-1] == 4) && ($hours < 12 || $hours > 14) )
+	{
+		return $hours_name = 'часа';
+	} 
+
+	return $hours_name = 'часов';
+}
+
+
+function show_time(){
+	list($hours, $minutes) = explode(':', date('H:i'));
+
+	echo "сейчас $hours ".get_hours_name($hours)." $minutes ".get_minutes_name($minutes);
 	
-		<div class="post">
-			<div class="details">
-				<h2><a href="#">Nunc commodo euismod massa quis vestibulum</a></h2>
-				<p class="info">posted 3 hours ago in <a href="#">general</a></p>
-			
-			</div>
-			<div class="body">
-				<p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>
-			</div>
-			<div class="x"></div>
-		</div>
-		
-		<div class="col">
-			<h3><a href="#">Ut enim risus rhoncus</a></h3>
-			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
-			<p>&not; <a href="#">read more</a></p>
-		</div>
-		<div class="col">
-			<h3><a href="#">Maecenas iaculis leo</a></h3>
-			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
-			<p>&not; <a href="#">read more</a></p>
-		</div>
-		<div class="col last">
-			<h3><a href="#">Quisque consectetur odio</a></h3>
-			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
-			<p>&not; <a href="#">read more</a></p>
-		</div>
-		
-		<div id="footer">
-			<p>Copyright &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
-		</div>	
-	</div>
-</body>
-</html>
+}
+
+show_time();
+
