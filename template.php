c30410d0e0ac95268eea92d4ad36cd3bdcfd6943
Vogusov <vogusov@gmail.com>
2020-08-30 22:14:54 +0300

    gallary 1

---
+++ template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gallary</title>
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+<div class="container">
+  
+  <h1>Photo gallary</h1>
+  <div class="gallary">
+  <?php
+  include "gallary.php";
+  ?>
+</div>
+
+</div>
+  
+</body>
+</html>
