c27419f3c3f827637b2555d9ad0489c2a567a425
Vogusov <vogusov@gmail.com>
2020-09-03 22:39:48 +0300

    full pass doesn't work

--- config.php
+++ config.php
@@ -2,3 +2,7 @@
 
 define("IMAGES_FOLDER", "./images/");
 define("SMALL_IMAGES_FOLDER", "./images-sm/");
+
+
+// define("IMAGES_FOLDER", $_SERVER['DOCUMENT_ROOT']."/images/");
+// define("SMALL_IMAGES_FOLDER", $_SERVER['DOCUMENT_ROOT']."/images-sm/");
